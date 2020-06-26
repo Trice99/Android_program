93f371114b70acda99866fa00484ac120e825754
Trice99 <40487258+Trice99@users.noreply.github.com>
2020-06-26 02:21:38 +0200

    Add files via upload

---
+++ app/test/java/com/example/skill_it_2021_09/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.skill_it_2021_09;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <OrdersFragment href="http://d.android.com/tools/testing">Testing documentation</OrdersFragment>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
